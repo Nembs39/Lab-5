dc3ebb509c7334391d04e0ad83462a0b867457bf
Nat <jamesmv@icloud.com>
2025-10-03 15:24:08 -0400

    did until question 27

---
+++ MapTester.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+/**
+ * Write a description of class MapTester here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MapTester
+{
+    // instance variables - replace the example below with your own
+    private HashMap<String, String> responseMap;
+    
+    /**
+     * Constructor for objects of class MapTester
+     */
+    public void enterNumber(String name, String number)
+    {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String lookupNumber(String name)
+    {
+        // put your code here
+        
+    }
+}
